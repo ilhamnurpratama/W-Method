ac5af4ecbed74f859ca87a91db09c102f2ee5768
ilhamnurpratama <ilham.nurpratama@gmail.com>
2022-12-10 20:16:35 +0700

    1.1 - MAJOR : Additional null column to continuous itterations

--- W.py
+++ W.py
@@ -1,35 +1,22 @@
 # W Program 
 # Created by Ilham Nur Pratama
-# Version 1.0
+# Version 1.1
 
 # Module Used
 import pandas as pd
 
-# List Used
-namaOutput_list = []
-nilaiOutput_list = []
-
 # Extract Dataframe
-# Data = pd.read_csv('data.csv')
 mData = pd.read_csv('data.csv')
-#mData = mData.set_index(mData['id'])
+mData = mData.set_index(mData['id'])
 oData = pd.read_csv('output.csv')
 oData = oData.set_index(oData['oid'])
 
 # Application UI
 print('W Method Program')
-print('Version 1.0')
+print('Version 2.0')
 print('Kelompok 7')
 print('Riset Operasi Lanjut - Teknik Industri S2UI')
 print('======================================================================')
-
-'''
-# Combine Data
-mData = data.merge(dataPayoff, how='outer', on='Branch')
-
-# Select Function
-fungsi = int(input('Masukan fungsi yang akan anda pilih (Max = 1, Min = 2): '))
-'''
 
 # Type Conversion
 mData['additionalPayoff'] = mData['additionalPayoff'].astype(float)
@@ -39,23 +26,44 @@ dataOutput = mData.loc[mData['type'] == 'output']
 nodeEvent = mData.loc[mData['type'] == 'event']
 nodeDecision = mData.loc[mData['type'] == 'decision']
 
-# First Step : Output Calculation
-# Calculating Logic
-mData['expectedPayoff'] = mData['RawPayoff'] * mData['probabilityBranch']
-mData['finalPayoff'] = mData['expectedPayoff'] + mData['additionalPayoff']
+# Main Logic
+nullColumn = mData.loc[mData['parent'].isnull()]
+notNullColumn = mData.loc[mData['parent'].notnull()]
 
-# Grouping Logic
-mData_valueOutput = mData.groupby(['type','parent'])['id','finalPayoff'].max()
-mData_valueOutput = mData_valueOutput.reset_index()
+for i in notNullColumn:
+    # Calculating Logic
+    mData['expectedPayoff'] = mData['RawPayoff'] * mData['probabilityBranch']
+    mData['finalPayoff'] = mData['expectedPayoff'] + mData['additionalPayoff']
+    
+    # Grouping Logic
+    mData_value = mData.groupby(['type','parent'])['id','finalPayoff','branch'].max()
+    mData_value = mData_value.reset_index()
+    mData_value = mData_value.set_index(mData_value['parent'])
+    mData['finalPayoff']=(mData_value['finalPayoff'])
+    mData['finalBranch']=(mData_value['branch'])
+        
+    # null Value calculation
+    nullRows = mData.loc[mData['parent'].notnull()]
+    
+    # Calculating Logic
+    nullRows['expectedPayoff'] = nullRows['finalPayoff'] * nullRows['probabilityBranch']
+    nullRows['finalPayoff'] = nullRows['expectedPayoff'] + nullRows['additionalPayoff']
+    
+    # Grouping Logic
+    nullRows_value = nullRows.groupby(['type','parent'])['id','finalPayoff','branch'].max()
+    nullRows_value = nullRows_value.reset_index()
+    nullRows_value = nullRows_value.set_index(nullRows_value['parent'])
+    
+    # Merge dataframe
+    mdf = pd.merge(mData,nullRows_value,left_index=True,right_index=True,how='outer')
+    mdf['finalPayoff_x'] = mdf['finalPayoff_x'].fillna(0)
+    mdf['finalPayoff_y'] = mdf['finalPayoff_y'].fillna(0)
+    mdf['finalPayoff'] = mdf.apply(lambda x: x['finalPayoff_x'] + x['finalPayoff_y'],axis=1)
 
-# Merging with parent
-mpData = pd.merge(mData,mData_valueOutput, left_on='id', right_on='parent')
-mpData.set_index('id_x')
-# mpData.to_csv('mpData.csv')
-
-# Event Calculation
-mpData['expectedPayoff'] = mpData['finalPayoff_y'] * mpData['probabilityBranch']
-mpData['finalPayoff'] = mpData['expectedPayoff'] + mpData['additionalPayoff']
-mpData_eventOutput = mpData.groupby(['type_x'])['id_x','finalPayoff'].max()
-mpData_eventOutput = mpData_eventOutput.reset_index()
-print(mpData_eventOutput)
+    #drop duplicated column
+    mdf = mdf.drop(['finalPayoff_x',
+                    'finalPayoff_y',
+                    'parent_y',
+                    'id_y',
+                    'branch_y',
+                    'type_y'],axis=1)
